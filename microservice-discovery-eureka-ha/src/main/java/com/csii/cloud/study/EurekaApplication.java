8e5a26b51120118c9f821d99de8badee9dec55f0
suzl <zhenglongsu@163.com>
2018-11-06 17:11:38 +0800

    microservice-discovery-eureka-ha over

--- microservice-discovery-eureka-ha/src/main/java/com/csii/cloud/study/EurekaApplication.java
+++ microservice-discovery-eureka-ha/src/main/java/com/csii/cloud/study/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.itmuch.cloud.study;
+package com.csii.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
