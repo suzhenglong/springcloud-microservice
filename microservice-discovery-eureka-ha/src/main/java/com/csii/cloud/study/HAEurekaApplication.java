e0934ee823273951899e36733763b574460bb035
suzl <zhenglongsu@163.com>
2018-11-07 14:08:04 +0800

    microservice-discovery-eureka-authenticating Eureka进行密码验证

--- microservice-discovery-eureka-ha/src/main/java/com/csii/cloud/study/HAEurekaApplication.java
+++ microservice-discovery-eureka-ha/src/main/java/com/csii/cloud/study/HAEurekaApplication.java
@@ -11,8 +11,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
   */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class HAEurekaApplication {
   public static void main(String[] args) {
-    SpringApplication.run(EurekaApplication.class, args);
+    SpringApplication.run(HAEurekaApplication.class, args);
   }
 }
