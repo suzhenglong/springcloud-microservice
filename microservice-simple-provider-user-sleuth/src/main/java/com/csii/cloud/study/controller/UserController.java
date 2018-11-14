4eab9600ead8bd196b59363aba1206b814c8ce3b
suzl <zhenglongsu@163.com>
2018-11-14 18:50:23 +0800

    整合spring-cloud-starter-sleuth

---
+++ microservice-simple-provider-user-sleuth/src/main/java/com/csii/cloud/study/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.csii.cloud.study.controller;
+
+import com.csii.cloud.study.entity.User;
+import com.csii.cloud.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+ /**
+  * @Description:
+  * @author: zhenglongsu@163.com
+  * @date: 2018.11.06 14:23
+  */
+@RestController
+public class UserController {
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/{id}")
+  public User findById(@PathVariable Long id) {
+    User findOne = this.userRepository.findOne(id);
+    return findOne;
+  }
+}
