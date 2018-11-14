820c93800649e3213e78f9273dbd63b51a63c998
suzl <zhenglongsu@163.com>
2018-11-14 17:54:30 +0800

    config 用户认证

---
+++ microservice-config-client-authenticating/src/main/java/com/csii/cloud/study/AuthenticatingConfigClientApplication.java
@@ -0,0 +1,11 @@
+package com.csii.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatingConfigClientApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AuthenticatingConfigClientApplication.class, args);
+  }
+}
