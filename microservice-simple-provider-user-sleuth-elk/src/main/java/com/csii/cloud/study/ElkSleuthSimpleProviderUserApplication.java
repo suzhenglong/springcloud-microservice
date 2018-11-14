1ad03839f022ba268d5448bba545dacb23eb4456
suzl <zhenglongsu@163.com>
2018-11-14 18:58:25 +0800

    Sleuth与ELK 配合使用

---
+++ microservice-simple-provider-user-sleuth-elk/src/main/java/com/csii/cloud/study/ElkSleuthSimpleProviderUserApplication.java
@@ -0,0 +1,14 @@
+package com.csii.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author suzl
+ */
+@SpringBootApplication
+public class ElkSleuthSimpleProviderUserApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ElkSleuthSimpleProviderUserApplication.class, args);
+  }
+}
