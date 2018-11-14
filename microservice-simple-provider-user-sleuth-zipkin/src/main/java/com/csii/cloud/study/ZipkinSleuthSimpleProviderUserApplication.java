1a64ac7223728d54622aa0b9ffaa00082684e1ec
suzl <zhenglongsu@163.com>
2018-11-14 19:24:22 +0800

    sleuth 与zipkin配合使用

---
+++ microservice-simple-provider-user-sleuth-zipkin/src/main/java/com/csii/cloud/study/ZipkinSleuthSimpleProviderUserApplication.java
@@ -0,0 +1,14 @@
+package com.csii.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author suzl
+ */
+@SpringBootApplication
+public class ZipkinSleuthSimpleProviderUserApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ZipkinSleuthSimpleProviderUserApplication.class, args);
+  }
+}
